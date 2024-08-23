86faf0d6e5219cf564a1085b2c2dc5d41d6612c1
Vivek <786vt786@gmail.com>
2024-08-23 22:26:01 +0530

    comment added

--- src/main/java/com/vivek/springBootApplication/Application.java
+++ src/main/java/com/vivek/springBootApplication/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		//Application start from here then it call controller
 	}
 
 }
